c58fb0cc58bd17880d0931a45090d7ccee02e783
Ayush1403 <ayushsrivastava03004@gmail.com>
2025-12-22 12:33:02 +0530

    changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,17 +4,17 @@ const Services = () => {
   return (
     <section className="services w-full z-20 bg-[#090909] h-dvh flex flex-col md:flex-row  relative items-center justify-center px-20">
       <div className="w-full md:w-1/2 flex justify-center">
-        <h1 className="text-[3rem] md:text-[5rem] text-primary  font-display font-stretch-90% tracking-tighter font-bold ">
+        <h1 className="text-[2rem] md:text-[5rem] text-primary  font-display font-stretch-90% tracking-tighter font-bold ">
           Hi! I’m{" "}
-          <span className="bg-primary text-cards px-2 rounded-lg">Ayush</span>,{" "}
+          <span className="bg-primary text-cards md:px-2 rounded-lg">Ayush</span>,{" "}
           <br />
           <span className="text-justify">
             I love building things that make the web feel simple and human.
           </span>
         </h1>
       </div>
-      <div className="flex items-center flex-col w-full md:w-1/2 text-[2rem] text-primary/80 md:px-15   font-display tracking-tighter font-normal ">
-        <p>
+      <div className="flex items-center flex-col w-full md:w-1/2 text-[1rem] text-primary/80 md:px-15   font-display tracking-tighter font-normal ">
+        <p className="hidden md:block">
           I’m a frontend developer based in India, focused on crafting clean,
         responsive, and intuitive web experiences using modern technologies like
         React and Tailwind CSS.
