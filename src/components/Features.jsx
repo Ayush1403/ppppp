7a1d15e784cc7ed4b0c8598f118a097c6f3b3786
Ayush1403 <ayushsrivastava03004@gmail.com>
2026-02-23 01:15:30 +0530

    new

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -45,13 +45,14 @@ const Features = ({projects}) => {
    <Link to={project.link} className='w-full'>
      <div className='w-full h-screen flex flex-col justify-center items-center bg-surface relative backdrop-blur-xl'>
       <div className='w-full h-[60%] md:h-full absolute' style={{backgroundColor: project.accentColor, filter: "blur(120px)", opacity: 0.04}}></div>
- <h1  ref={(el) => (titleRefs.current[index] = el)} className='text-2xl heading font-main uppercase absolute text-primary/10 tracking-wider text-[clamp(2rem,20vw,100vw)] font-black z-10'>{project.title}.{project.title}.{project.title}.</h1>
+ <h1  ref={(el) => (titleRefs.current[index] = el)} className='text-2xl heading font-main uppercase absolute bottom-30 text-primary/10 tracking-wider text-[clamp(35vw,20vw,100vw)] font-black z-10'>{project.title}.{project.title}.{project.title}.</h1>
 
-      <div className='w-[80%] h-[80%] relative flex justify-center items-center border-2 rounded-3xl border-white/10  gap-10'>
+      <div className='w-[80%] h-[80%]  relative flex justify-center items-center border-2 rounded-3xl border-white/10  gap-10'>
  <h1 className='text-2xl font-main uppercase tracking-wider text-[clamp(2rem,10vw,6vw)] font-black z-10'>{project.title}</h1>
- <div className='w-15 h-15 border-b-3 border-r-3 absolute bottom-20 right-20 border-accent'></div>
-  <h1 className='absolute top-20 left-20 text-[14px] text-white/20 font-main font-black md:text-4xl'>Click TO See Demo</h1>
+ <div className='w-15 h-15 border-b-3 border-r-3  hidden md:block absolute bottom-20 right-20 border-accent'></div>
+  <h1 className='absolute top-20 left-20 hidden md:block text-[14px] text-white/20 font-main font-black md:text-4xl'>Click TO See Demo</h1>
  <h1 className='absolute bottom-20 left-20 text-white/20 font-main font-black text-4xl'>{project.id}</h1>
+
  <div className='w-15 h-15 border-t-3 border-r-3 absolute top-20 right-20 border-accent'></div>
       </div>
     </div></Link>
